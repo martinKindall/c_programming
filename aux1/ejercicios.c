60a82cc814fe16a15a716c8a019bea614ed03af5
martinKindall <martin@rflex.cl>
2018-03-24 15:13:22 -0300

    funcion que determina el nro de bits en 1 de un entero

--- aux1/ejercicios.c
+++ aux1/ejercicios.c
@@ -20,12 +20,29 @@ int sizeOfInt()
 	return size * bits_in_byte;
 }
 
+int bits1(int n)
+{
+	int bits1 = 0;
+	int mask1 = 0x0001;
+
+	while(n)
+	{
+		if (n & mask1) ++bits1;
+		n >>= 1;
+	}
+
+	return bits1;
+}
+
 
 int main()
 {
 	int num = 0;
 	printf("tamaño %lu bytes\n", sizeof(num));
 	printf("tamaño custom %d bits\n", sizeOfInt());
+
+	int num2 = 0xFF;
+	printf("nro %d tiene %d bits en 1\n", num2, bits1(num2));
 	
 	return 0;
 }
