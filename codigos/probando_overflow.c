0158e861ac52da5ca58c48126536007698b0d34d
martinKindall <martin@rflex.cl>
2018-03-24 13:52:49 -0300

    pruebas de overflow de int y conversion double

--- codigos/probando_overflow.c
+++ codigos/probando_overflow.c
@@ -12,5 +12,17 @@ int main()
 	printf("El resultado int es %d\n", res);
 	// 128, no hay problema
 
+	/* ------  */
+
+	int a = 2147483647;
+	double x = a + 1;
+	printf("El resultado double es %f\n", x);
+	// overflow, -2147483648.000000
+	
+	int b = 2147483647;
+	double y = b + 1.0;  // aca realiza una suma double, se elimina el problema
+	printf("El resultado double es %f\n", y);
+	// 2147483648.000000
+
 	return 0;	
 }
