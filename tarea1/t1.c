187dcb247f091a134a43c77eb12ac77f1aee7407
martinKindall <martin@rflex.cl>
2018-04-15 16:33:23 -0300

    Se eliminan comentarios de la t1

--- tarea1/t1.c
+++ tarea1/t1.c
@@ -2,35 +2,6 @@
 #include <string.h>
 #include "t1.h"
 
-// int main()
-// {
-// 	uint res = recortar(0b011010101111010100101100, 19, 8);
-// 	// uint res = recortar(0xffffffff, 0, 0);
-
-// 	uint testRes = 0b011000101100;
-// 	if (res == testRes)
-// 	{
-// 		printf("paso test\n");
-// 	}
-
-// 	char *str1 = "holanda ";
-// 	char strAux[strlen(str1) + 1];
-// 	strcpy(strAux, str1);
-// 	printf("%s\n", strAux);
-// 	shiftStringRight(strAux);
-// 	printf("%s\n", strAux);
-// 	shiftStringRight(strAux);
-// 	printf("%s\n", strAux);
-// 	shiftStringRight(strAux);
-// 	printf("%s\n", strAux);
-// }
-
-// int main()
-// {
-// 	char test1[]= "mhello   \0f";
-// 	alinear_der(test1);
-// }
-
 uint recortar(uint x, int i, int j)
 {
 	uint auxL = (x >> i) >> 1;
