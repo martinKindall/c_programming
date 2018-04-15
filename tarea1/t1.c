412c59d58a897e219aa97d4463b7266cf389cdf7
martinKindall <martin@rflex.cl>
2018-04-15 16:20:38 -0300

    Terminada Tarea 1

--- tarea1/t1.c
+++ tarea1/t1.c
@@ -12,6 +12,23 @@
 // 	{
 // 		printf("paso test\n");
 // 	}
+
+// 	char *str1 = "holanda ";
+// 	char strAux[strlen(str1) + 1];
+// 	strcpy(strAux, str1);
+// 	printf("%s\n", strAux);
+// 	shiftStringRight(strAux);
+// 	printf("%s\n", strAux);
+// 	shiftStringRight(strAux);
+// 	printf("%s\n", strAux);
+// 	shiftStringRight(strAux);
+// 	printf("%s\n", strAux);
+// }
+
+// int main()
+// {
+// 	char test1[]= "mhello   \0f";
+// 	alinear_der(test1);
 // }
 
 uint recortar(uint x, int i, int j)
@@ -23,6 +40,37 @@ uint recortar(uint x, int i, int j)
 	return (auxL << j) | auxR;
 }
 
-void alinear_der(char *str){
+void shiftStringRight(char *str)
+{
+	int largo = strlen(str);
+	char *origin = str;
+	char aux[strlen(str)+1];
+	char *auxP = aux;
+	*auxP++ = ' ';
 
+	for (int i = 0; i < largo-1; ++i)
+	{
+		*auxP = *str;
+		++auxP;
+		++str;
+	}
+	*auxP = 0;
+
+	strcpy(origin, aux);
 }
+
+void alinear_der(char *str){
+	int largo = strlen(str);
+	char auxCopy[strlen(str) + 1];
+	strcpy(auxCopy, str);
+
+	char *cola = auxCopy + largo - 1;
+
+	while(largo && (*cola == ' '))
+	{
+		shiftStringRight(str);
+		--cola;
+		--largo;
+	}
+}
+
