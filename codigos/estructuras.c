16fedceff54c16392588731f5e16b4ca4a88c605
martinKindall <martin@rflex.cl>
2018-05-07 18:20:13 -0300

    Probando arreglos como parametros en funciones

--- codigos/estructuras.c
+++ codigos/estructuras.c
@@ -4,6 +4,9 @@ typedef struct punto{
 	float x,y;
 }Punto;
 
+int devuelve(int a[]){
+	return a[0];
+}
 
 int main(){
 	Punto pa = {0,10};
@@ -13,4 +16,11 @@ int main(){
 
 	printf("La coordenada x es %f\n", pa.x);
 	printf("La coordenada x es %f\n", pc->x);
+
+	int numeros[] = {1,2,3,4};
+	int* arrNum = malloc(sizeof(int)*2);
+	arrNum[0] = 1;
+	arrNum[1] = 50;
+
+	printf("%d\n", devuelve(arrNum));
 }
