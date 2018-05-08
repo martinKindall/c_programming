52a38f8a4d9f537cf197ffaaf844cd4f45777dea
martinKindall <martin@rflex.cl>
2018-05-07 22:53:25 -0300

    Se encuentra logica para rescatar el numero de la linea

--- tarea2/definir.c
+++ tarea2/definir.c
@@ -3,6 +3,8 @@
 #include <string.h>
 
 #define LINE_WIDTH 100
+#define KEY_WIDTH 20
+#define INDEX_WIDTH 10
 
 void read_line(FILE *file, char *line, int i) {
   if (fseek(file, i*LINE_WIDTH, SEEK_SET)!=0) {
@@ -19,7 +21,49 @@ void trimwhitespace(char *str)
 {
 	char *p = strchr(str, ' ');
 	if (!p)
+	{
+		return;
+	}
 	*p = 0;
+}
+
+void definirKeyABB(FILE *file, int linea, char *key)
+{
+	int nodo;
+	char busqueda[LINE_WIDTH];
+	char* pBusqueda = busqueda;
+
+    read_line(file, busqueda, linea);
+
+    char searchKey[KEY_WIDTH];
+    strncpy(searchKey, pBusqueda + 2*INDEX_WIDTH, KEY_WIDTH);
+    trimwhitespace(searchKey);
+
+    char leftNode[INDEX_WIDTH];
+    strncpy(leftNode, pBusqueda, INDEX_WIDTH);
+
+    char rightNode[INDEX_WIDTH];
+    strncpy(rightNode, pBusqueda + INDEX_WIDTH, INDEX_WIDTH);
+
+    int cmp = strcmp(searchKey, key);
+
+    if (cmp == 0)
+    {
+    	printf("redefinir\n");
+    }
+    else if(cmp > 0){
+    	printf("es mayor\n");
+    	nodo = atoi(leftNode);
+    	printf("El nodo es: %d\n", nodo);
+    }
+    else
+    {
+    	printf("es menor\n");
+    	nodo = atoi(rightNode);
+    	printf("El nodo es: %d\n", nodo);
+    }
+
+    printf("Raiz: %s\n", searchKey);
 }
 
 int main(int argc, char **argv) {
@@ -32,21 +76,7 @@ int main(int argc, char **argv) {
 	char *key= argv[2];
 	char *value= argv[3];
 
-	printf("%s\n", filename);
-	printf("%s\n", key);
-	printf("%s\n", value);
-
 	FILE *file= fopen(filename, "r+");
 
-	char busqueda[LINE_WIDTH];
-	char* pBusqueda = busqueda;
-
-    read_line(file, busqueda, 0);
-
-    char searchKey[20];
-    strncpy(searchKey, pBusqueda + 20, 20);
-    trimwhitespace(searchKey);
-
-    printf("%s\n", busqueda);
-    printf("%s\n", searchKey);
+	definirKeyABB(file, 0, key);
 }
