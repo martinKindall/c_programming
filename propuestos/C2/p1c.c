43013f9ae57e8369fbb8f667c01a0d7530d43e98
martinKindall <martin@rflex.cl>
2018-07-03 21:36:13 -0400

    Segundo intento de solucion prop C2

--- propuestos/C2/p1c.c
+++ propuestos/C2/p1c.c
@@ -9,17 +9,17 @@ typedef struct params{
   double xi, xf, yi, yf;
   int n, m;
   double* res;
-  pthread_mutex_t mutex;
+  pthread_mutex_t* mutex;
 } Param;
 
 void* fun_par(void *ptr)
 {
 	Param* params = (Param*) ptr;
-	// double resAux = integral_g_dx_dy(params->xi, params->xf, params->n, params->yi, params->yf, params->m);
+	double resAux = integral_g_dx_dy(params->xi, params->xf, params->n, params->yi, params->yf, params->m);
 	
-	// pthread_mutex_lock(&(params->mutex));	
-	// *(params->res) = *(params->res) + resAux;
-	// pthread_mutex_unlock(&(params->mutex));	
+	pthread_mutex_lock(params->mutex);	
+	*(params->res) = *(params->res) + resAux;
+	pthread_mutex_unlock(params->mutex);	
 
 	return NULL;
 }
@@ -27,24 +27,16 @@ void* fun_par(void *ptr)
 double integral_g_dx_dy_par(double xi, double xf, int n,
                             double yi, double yf, int m)
 {
-	int cores = 8;
+	int cores = 2;
 	pthread_t hilos[cores];
 	pthread_mutex_t mutex;
 	pthread_mutex_init(&mutex, NULL);
-	double resultado = 0;
+	double resultado;
 
 	for (int i = 0; i < cores; ++i)
 	{
-		Param* parametros = (Param*) malloc(sizeof(struct params));
-		parametros->xi = xi;
-		parametros->xf = xf;
-		parametros->n = n;
-		parametros->mutex = mutex;
-		parametros->res = &resultado;
-		parametros->yi = yi/cores * (i);
-		parametros->yf = yi/cores * (i+1);
-		parametros->m = m/cores;
-		pthread_create(&hilos[i], NULL, fun_par, (void*) parametros);
+		Param parametros = {xi, xf, yi + (yf - yi)/cores * (i), yi + (yf - yi)/cores * (i+1), n, m/cores, &resultado, &mutex};
+		pthread_create(&hilos[i], NULL, fun_par, (void*) &parametros);
 	}
 
 	for (int i = 0; i < cores; ++i)
