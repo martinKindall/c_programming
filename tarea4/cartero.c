d987f5583b665b29186e4d12830637e7771849a7
martinKindall <martin@rflex.cl>
2018-08-18 13:59:29 -0300

    Se configura servidor de conexion mediante variable de entorno

--- tarea4/cartero.c
+++ tarea4/cartero.c
@@ -5,6 +5,8 @@
 #include <stdint.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <signal.h>
+#include <string.h>
 
 #include "jsocket.h"
 #include "util.h"
@@ -18,6 +20,15 @@ void mostrarUso()
 	exit(1);
 }
 
+
+void segFault()
+{
+	printf("Debe exportar la variable de entorno con la informacion del servidor\n");
+	printf("CORREO=servidor.com:3000\n");
+	printf("Si el problema persiste contactarse con el desarrollador.\n");
+	exit(1);
+}
+
 int main(int argc, char** argv)
 {
 	if (argc > 4 || argc < 1)
@@ -25,15 +36,24 @@ int main(int argc, char** argv)
 		mostrarUso();
 	}
 
-	char modo = *argv[1];
+	signal(SIGSEGV, segFault);
+
+	char* var_correo = getenv("CORREO");
+
+	char* split = strtok(var_correo, ":");
+	char* dominio = split;
+	split = strtok(NULL, ":");
+	int port = atoi(split);
 
 	int s = j_socket();
 
-	if (j_connect(s, "localhost", 3000) < 0)
+	if (j_connect(s, dominio, port) < 0)
 	{
 		printf("Error de conexion\n");
 		exit(1);
 	}
+
+	char modo = *argv[1];
 
 	write(s, &modo, 1);
 
